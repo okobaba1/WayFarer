abb39953ebada87326a3af7a337a92ccc025dae3
okobaba <okobaba2013@gmail.com>
2019-07-07 01:04:04 +0100

    chore: setup travis [Finishes 167065548]

--- test/tests.js
+++ test/tests.js
@@ -10,45 +10,45 @@ should();
 
 
 describe('User', ()=> {
-  // it('should sign up a user', (done) => {
-  //   const user = {
-  //     email: 'victor@gmail.com',
-  //     first_name: 'moke',
-  //     last_name: 'ilo',
-  //     password: '1234hdgdpds',
-  //   };
-  //   chai.request(app)
-  //     .post('/api/v1/auth/signup')
-  //     .send(user)
-  //     .end((err, res) => {
-  //       res.should.have.status(201);
-  //       expect(res.body).be.an('object');
-  //       expect(res.body.status).be.a('number');
-  //       expect(res.body.data).be.an('object');
-  //       assert.equal(res.body.status, 201);
-  //       done();
-  //     });
-  // });
-  // it('User already exists', (done) => {
-  //   const user = {
-  //     email: 'victor@gmail.com',
-  //     first_name: 'moke',
-  //     last_name: 'ilo',
-  //     password: '1234hdgdpds',
-  //   };
-  //   chai.request(app)
-  //     .post('/api/v1/auth/signup')
-  //     .send(user)
-  //     .end((err, res) => {
-  //       res.should.have.status(409);
-  //       expect(res.body).be.an('object');
-  //       expect(res.body.status).be.a('number');
-  //       expect(res.body.error).be.an('string');
-  //       assert.equal(res.body.status, 409);
-  //       assert.equal(res.body.error, 'User already exists');
-  //       done();
-  //     });
-  // });
+  it('should sign up a user', (done) => {
+    const user = {
+      email: 'victor@gmail.com',
+      first_name: 'moke',
+      last_name: 'ilo',
+      password: '1234hdgdpds',
+    };
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(user)
+      .end((err, res) => {
+        res.should.have.status(201);
+        expect(res.body).be.an('object');
+        expect(res.body.status).be.a('number');
+        expect(res.body.data).be.an('object');
+        assert.equal(res.body.status, 201);
+        done();
+      });
+  });
+  it('User already exists', (done) => {
+    const user = {
+      email: 'victor@gmail.com',
+      first_name: 'moke',
+      last_name: 'ilo',
+      password: '1234hdgdpds',
+    };
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(user)
+      .end((err, res) => {
+        res.should.have.status(409);
+        expect(res.body).be.an('object');
+        expect(res.body.status).be.a('number');
+        expect(res.body.error).be.an('string');
+        assert.equal(res.body.status, 409);
+        assert.equal(res.body.error, 'User already exists');
+        done();
+      });
+  });
   it('invalid input', (done) => {
     const user = {
       email: 'victor@gmail.com',
