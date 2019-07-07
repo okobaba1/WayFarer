7dfd98fdb59aac32cc13e1bbed57cb29ce4c69d9
okobaba <okobaba2013@gmail.com>
2019-07-07 01:03:33 +0100

    chore: setup travis [Finishes 167065548]

--- test/tests.js
+++ test/tests.js
@@ -1,6 +1,6 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import app from '../server/app';
+import app from '../server/database/dbconnection';
 
 const {
   expect, assert, should,
